139bfd701f8924aea730a5b988337d5fb7db8f9a
inasadil22-ai <inasadil22@gmail.com>
2026-06-28 05:47:40 +0500

    chore: remove unused duplicate XAIInsights.jsx in components/

--- backend/test_atlas.py
+++ backend/test_atlas.py
@@ -2,7 +2,7 @@
 from pymongo.mongo_client import MongoClient
 from pymongo.server_api import ServerApi
 
-uri = "mongodb+srv://factfusion_user:qwertyuiop1234567890@cluster0.lgojzft.mongodb.net/?appName=Cluster0"
+uri = "mongodb+srv://factfusion_user:clbXT6tcQUdKIOdb@cluster0.lgojzft.mongodb.net/?appName=Cluster0"
 
 # Create a new client and connect to the server
 client = MongoClient(uri, server_api=ServerApi('1'))
